106e3f1a2c0e2f1bcdfc7371852ed1e46eb547f8
cjkolb <cjkolb27@gmail.com>
2025-07-08 22:06:40 -0400

    [Implemented] added calendar data collection

--- CalendarDatabase.py
+++ CalendarDatabase.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import os
 
 def handleClientRecieving(connId, end):
     
@@ -7,31 +8,49 @@ def handleClientRecieving(connId, end):
     clientPort = ""
     client = None
     firstTime = True
+    data = ""
 
     while(end[0]):
         try:
-            data = connId.recv(1024)
+            data = connId.recv(1024).decode()
+            if not data:
+                print(f"Client {clientHost} has left the server.\n")
+                if client != None:
+                    with clients_lock:
+                        client.close
+                        clients.remove(client)
+                break
+            while "*]*END*[*\r\n\r\n" not in data:
+                sent = connId.recv(1024).decode()
+                if not sent or not data:
+                    if client != None:
+                        with clients_lock:
+                            client.close
+                            clients.remove(client)
+                    return
+                data += sent
+
         except Exception as e:
-            print(f"Client {clientHost} has left the server.\n")
+            print(f"{e}\r\nClient {clientHost} has left the server.\n")
             if client != None:
-                client.close
-                clients.remove(client)
+                with clients_lock:
+                    client.close
+                    clients.remove(client)
             break
 
         if not end[0]:
             break
         
-        if not data:
-            print(f"Client {clientHost} has left the server.\n")
-            break
-        
-        data = data.decode()
         print("\r\n" + data)
         parse = data.split("\r\n")
         try:
             line1 = parse[0].split(" ")
             if len(line1) != 2 and line1[1] != version():
                 print(f"Client {clientHost} has left the server.\n")
+                if client != None:
+                    with clients_lock:
+                        client.close
+                        clients.remove(client)
                 break
             if line1[0] == "Post" and firstTime:
                 firstTime = False
@@ -39,6 +58,10 @@ def handleClientRecieving(connId, end):
                 line3 = parse[2].split(" ")
                 if len(line2) != 2 or line2[0] != "Host:":
                     print(f"Client {clientHost} has left the server.\n")
+                    if client != None:
+                        with clients_lock:
+                            client.close
+                            clients.remove(client)
                     break
                 clientHost = line2[1]
                 clientPort = line3[1]
@@ -47,26 +70,44 @@ def handleClientRecieving(connId, end):
                     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     client.connect((socket.gethostbyname(clientHost), int(clientPort)))
                     clients.append(client)
-                broadcast() 
+                broadcast("Hello, another client has join the server.") 
             
             if line1[0] == "Put":
                 line2 = parse[1].split(" ")
                 if len(line2) != 2 or line2[0] != "Host:":
                     print(f"Client {clientHost} has left the server.\n")
                     break
+
         except:
             break
     return
 
-def broadcast():
+def compareCalendar(client, file, changes):
+    try:
+        os.mkdir("CalendarDatabase")
+    except FileExistsError:
+        print("CalendarDatabase file directory exists.")
+    except Exception as e:
+        print(f"{e}")
+        return
+    
+
+    return
+
+def broadcast(message):
+    print(f"\r\nBroadcasted: {message}\r\n")
     with clients_lock:
         for client in clients:
-            client.sendall("Hello this is a test broadcast\r\n\r\n".encode())
+            try:
+                client.sendall(f"{message}\r\n\r\n".encode())
+            except Exception as e:
+                print(e)
     return
 
 def serverInputHandler(end):
     cmd = input("")
     while(cmd != "Quit"):
+        broadcast(cmd)
         cmd = input("")
     end[0] = False
     serverSocket.close()
